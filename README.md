bc3cdaa8002f15b5d534ada5e9c743e6731c6b12
LGazola <luizgazolafortuna@gmail.com>
2024-04-12 14:14:32 -0300

    modificado o README.md e feito a atividade da trilha (soma, ímpar ou par e string)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TrilhaTads
 repositório para a trilha de github
+
+# primeiro commit
+
+commit feito estudando as trilhas para prova
---
+++ primeiroarquivo.js
@@ -0,0 +1,19 @@
+let a = 3
+let b = 6
+let soma = a + b
+console.log(soma)
+
+if (soma % 2 === 0){
+    console.log ("par")
+    
+}else{console.log("ímpar")}
+
+let string = "eu trabalho cortando grama"
+console.log(string)
+console.log (string.length)
+console.log(string.toUpperCase())
+let palavras = string.split(" ");
+for (let i = 0; i < palavras.length; i++) {
+    console.log(palavras[i]);
+}
+
